9c019b2406151dceefd0a396431ae6c2ee179286
saeid pirzad <saeid.pirzad93@yahoo.com>
2024-09-04 16:39:19 +0430

    create sideBar in Xl desplay

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { initFlowbite } from 'flowbite';
 
 @Component({
@@ -9,7 +9,16 @@ import { initFlowbite } from 'flowbite';
 export class AppComponent implements OnInit {
   title = 'dashboard';
 
-
+  variable =false
+  @HostListener('document:scroll')
+  scrollFun(){
+    if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+      this.variable = true
+    }
+    else{
+      this.variable =false
+    }
+  }
   ngOnInit(): void {
     initFlowbite();
   }
